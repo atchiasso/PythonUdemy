bedff336f27d29f3bbc5962dda407cd660944316
armelito60 <tchiasso@et.esiea.fr>
2022-05-03 15:30:59 +0200

    traçage de plusieurs carrés via la fonction carres()

--- programme_tortue/main.py
+++ programme_tortue/main.py
@@ -7,7 +7,16 @@ def escalier(taille, nb):
         t.forward(taille)
         t.right(90)
     t.forward(taille)
+def carres(taille_depart, nb):
+    for i in range(0,nb):
+        carre((i+1)*taille_depart)
+
+def carre(taille):
+    for i in range(0, 4):
+        t.forward(taille)
+        t.left(90)
 
 t = turtle.Turtle()
-escalier(50,10)
+#escalier(60,5)
+carres(50, 6)
 turtle.done()
