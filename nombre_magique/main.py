7aaa85efbd34db060611341099a8f49d271ee659
armelito60 <tchiasso@et.esiea.fr>
2022-05-03 16:22:58 +0200

    ajout du nombre de vies limitées pour trouver le nombre magique

--- nombre_magique/main.py
+++ nombre_magique/main.py
@@ -4,6 +4,7 @@ import random
 def demander_nombre(nb_min, nb_max):
     nombre_int = 0
     while nombre_int == 0:
+        print(f"Il vous restes {vies} vies")
         number = input(f"Quel est le nombre magique ? (entre {nb_min} et {nb_max})  ")
         try:
             nombre_int = int(number)
@@ -19,13 +20,20 @@ def demander_nombre(nb_min, nb_max):
 NOMBRE_MIN = 1
 NOMBRE_MAX = 10
 NOMBRE_MAGIQUE = random.randint(NOMBRE_MIN,NOMBRE_MAX)
+NB_VIES = 4
 
 nombre = 0
-while nombre != NOMBRE_MAGIQUE:
+vies = NB_VIES
+while nombre != NOMBRE_MAGIQUE and vies > 0:
     nombre = demander_nombre(NOMBRE_MIN, NOMBRE_MAX)
     if nombre < NOMBRE_MAGIQUE:
         print("Le nombre magique est plus grand")
+        vies -= 1
     elif nombre > NOMBRE_MAGIQUE:
         print("Le nombre magique est plus petit")
+        vies -= 1
     else:
         print("Bravo, vous avez gagné !")
+
+if vies == 0:
+    print(f"Vous avez perdu ! Le nombre magique était {NOMBRE_MAGIQUE}")
