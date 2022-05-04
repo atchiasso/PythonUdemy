dc1df62ac71c966cc755b1051ebd641bdd36acde
armelito60 <tchiasso@et.esiea.fr>
2022-05-04 14:42:39 +0200

    refactorisation du code

---
+++ fonction_exo/main.py
@@ -0,0 +1,8 @@
+def recuperer_et_afficher_infos_programme(numero):
+    nom = input("Nom de la personne " + str(numero) + ": ")
+    age = input("Age de la personne " + str(numero) + ": ")
+    print("La personne", numero, "est", nom, "son age est", age, "ans")
+
+recuperer_et_afficher_infos_programme(1)
+recuperer_et_afficher_infos_programme(2)
+recuperer_et_afficher_infos_programme(3)
