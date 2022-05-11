18eea57e825b516f0af31ae3d151dda143907c16
armelito60 <tchiasso@et.esiea.fr>
2022-05-11 10:34:06 +0200

    utilisation de lambda pour table d'opérations

--- fonction_exo/callback.py
+++ fonction_exo/callback.py
@@ -1,16 +1,7 @@
-def mult_callback(a,b):
-    return a*b
-
-def add_callback(a,b):
-    return a+b
-
-def power_callback(a,b):
-    return pow(a, b)
-
 def afficher_table(n, operateur_str, operation_cbk):
     for i in range(1, 10):
         print(i, operateur_str, n, "=", operation_cbk(i, n))
 
-afficher_table(2, "+", add_callback)
+afficher_table(2, "*", lambda a, b : a * b)
 print()
-afficher_table(2, "^", power_callback)
+afficher_table(2, "^", lambda a, b : pow(a,b))
