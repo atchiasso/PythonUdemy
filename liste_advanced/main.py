6f78642c86ed61a61d6d5134a49deb50b9482b09
armelito60 <tchiasso@et.esiea.fr>
2022-05-20 15:39:35 +0200

    exercice trouver un element dans une liste

--- liste_advanced/main.py
+++ liste_advanced/main.py
@@ -1,15 +1,18 @@
 # LES COLLECTIONS : LISTES / TUPLES
 # Append / Extend / += / Insert
 
+
+
+def element_dans_liste(e, l):
+    e = e.lower()
+    l = [i.lower() for i in l]
+    if e in l:
+        return True
+    else:
+        return False
 noms = ["Jean", "Sophie", "Martin", "Christophe", "Zoe"]
 
-# longueurs_noms = [len(nom) if len(nom) < 10 else 0 for nom in noms]
-# nom_avec_e = [nom for nom in noms if "e" in nom]
-
-# a = [i for i in range(5) if (i % 2) == 0]
-# b = [(i, True if (i%2) == 0 else False) for i in range(5)]
-# print(b)
-
-nom = "toto464"
-digit = any([c.isdigit() for c in nom])
-print(digit)
+if element_dans_liste("JEAn", noms):
+    print("Jean est là")
+else:
+    print("Jean n'est pas là")
